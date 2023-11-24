e879fd03c6b7d2c3d1e2eb0ec25cf802049875a1
Panji97 <panjisetyokurniawan@gmail.com>
2023-11-25 00:07:42 +0700

    belajar console.log()

---
+++ pertemuan1/console.log.js
@@ -0,0 +1,6 @@
+//console.log() adalah sebuah function yang digunakan untuk menampilkan data di console pada sisi client
+
+console.log('ini string')
+console.log(1 + 1)
+console.log(1 + '1')
+console.log('wadespo')
