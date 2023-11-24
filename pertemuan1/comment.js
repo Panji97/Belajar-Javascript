0490878647a6954540a51ead5939836f8b798145
Panji97 <panjisetyokurniawan@gmail.com>
2023-11-25 00:01:49 +0700

    belajar comment

---
+++ pertemuan1/comment.js
@@ -0,0 +1,6 @@
+//kita akan belajar comment
+//comment adalah syntax yang digunakan agar program tidak dieksekusi
+//comment terbagi menjadi 2 yaitu
+
+// const username = 'Panji Ganteng banget'
+/* console.log(username) */
