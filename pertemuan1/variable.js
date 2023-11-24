504fb71dc195da5232c723010e733875f88904e2
Panji97 <panjisetyokurniawan@gmail.com>
2023-11-24 23:56:36 +0700

    first commit

---
+++ pertemuan1/variable.js
@@ -0,0 +1,18 @@
+//varible digunakan untuk menyimpan data
+//variable 3 (var, let dan const)
+
+// var
+var username = 'panji'
+var angka = 100000
+console.log(username)
+console.log(angka)
+
+// let
+let fullname = 'wadespo'
+let angkaGenap = 5000000
+console.log(fullname)
+console.log(angkaGenap)
+
+// const
+const namaLengkap = 'Panji setyo kurniawan ganteng banget'
+console.log(namaLengkap)
